577b0a13fada9d73a3930b53bd3218674b0fbcc2
ss0900 <psypsy1234zz@gmail.com>
2026-01-29 16:56:25 +0900

    Updated Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,11 @@ function Loader({ isLoading }) {
     <div className={`loader ${!isLoading ? 'hidden' : ''}`}>
       <div className="loader-content">
         <div className="loader-logo">
-          NEXON<span style={{ opacity: 0.6 }}>Tech</span>
+          <img
+            src="/피식스에스씨_rgb_03.상하국문.png"
+            alt="Company Logo"
+            style={{ height: '4rem', objectFit: 'contain' }}
+          />
         </div>
         <div className="loader-bar">
           <div className="loader-progress" />
